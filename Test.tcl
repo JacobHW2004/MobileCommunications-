a82762deff5a002e9a834a0645d22525bc5c67ed
JacobHW2004 <110376542+JacobHW2004@users.noreply.github.com>
2025-02-07 17:00:12 +0000

    Update Test.tcl

--- Test.tcl
+++ Test.tcl
@@ -1,98 +1,124 @@
-# Create a new simulator
-set ns [new Simulator]
+# wrls1.tcl
+# A 3-node example for ad-hoc simulation with DSDV
+# By Dr. Idris Skloul Ibrahim
 
-# Set up the wireless channel
-set channel [new Channel/WirelessChannel]
+# Define options
+set val(chan)           Channel/WirelessChannel    ;# channel type
+set val(prop)           Propagation/TwoRayGround   ;# radio-propagation model
+set val(netif)          Phy/WirelessPhy            ;# network interface type
+set val(mac)            Mac/802_11                 ;# MAC type
+set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type
+set val(ll)             LL                         ;# link layer type
+set val(ant)            Antenna/OmniAntenna        ;# antenna model
+set val(ifqlen)         50                         ;# max packet in ifq
+set val(nn)             3                          ;# number of mobilenodes
+set val(rp)            AODV                       ;# AODV routing protocol
+#set val(rp)             DSDV                       ;# DSDV routing protocol
+set val(x)              500   			  		   ;# X dimension of topography
+set val(y)              400   			   		   ;# Y dimension of topography  
+set val(stop)		    150			   			   ;# time of simulation end
 
-# Define the general operational parameters
-set prop [new Propagation/TwoRayGround]
-set netif [new Phy/WirelessPhy]
-set mac [new Mac/802_11]
-set ifq [new Queue/DropTail/PriQueue]
-set ll [new LL]
-set ant [new Antenna/OmniAntenna]
-set god [new God]
+set ns		  [new Simulator]
+set tracefd       [open $val(rp)_trace_file_3_nodes.tr w]
+set windowVsTime2 [open win.tr w] 
+set namtrace      [open simwrls.nam w]    
 
-# Open the NAM trace file
-set nf [open out.nam w]
-$ns namtrace-all-wireless $nf 500 500  ;# Define a 500x500 simulation area
+$ns trace-all $tracefd
+$ns namtrace-all-wireless $namtrace $val(x) $val(y)
 
-# Define a 'finish' procedure
-proc finish {} {
-    global ns nf
+# set up topography object
+set topo       [new Topography]
+
+$topo load_flatgrid $val(x) $val(y)
+
+create-god $val(nn)
+
+#
+#  Create nn mobilenodes [$val(nn)] and attach them to the channel. 
+#
+
+# configure the nodes
+        $ns node-config -adhocRouting $val(rp) \
+			 -llType $val(ll) \
+			 -macType $val(mac) \
+			 -ifqType $val(ifq) \
+			 -ifqLen $val(ifqlen) \
+			 -antType $val(ant) \
+			 -propType $val(prop) \
+			 -phyType $val(netif) \
+			 -channelType $val(chan) \
+			 -topoInstance $topo \
+			 -agentTrace ON \
+			 -routerTrace ON \
+			 -macTrace OFF \
+			 -movementTrace ON
+			 
+	for {set i 0} {$i < $val(nn) } { incr i } {
+		set node_($i) [$ns node]	
+	}
+
+# Provide initial location of mobilenodes
+$node_(0) set X_ 5.0
+$node_(0) set Y_ 5.0
+$node_(0) set Z_ 0.0
+
+$node_(1) set X_ 490.0
+$node_(1) set Y_ 285.0
+$node_(1) set Z_ 0.0
+
+$node_(2) set X_ 150.0
+$node_(2) set Y_ 240.0
+$node_(2) set Z_ 0.0
+
+# Generation of movements
+$ns at 10.0 "$node_(0) setdest 250.0 250.0 3.0"
+$ns at 15.0 "$node_(1) setdest 45.0 285.0 5.0"
+$ns at 110.0 "$node_(0) setdest 480.0 300.0 5.0" 
+
+# Set a TCP connection between node_(0) and node_(1)
+set tcp [new Agent/TCP/Newreno]
+$tcp set class_ 2
+set sink [new Agent/TCPSink]
+$ns attach-agent $node_(0) $tcp
+$ns attach-agent $node_(1) $sink
+$ns connect $tcp $sink
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ns at 10.0 "$ftp start" 
+
+# Printing the window size
+proc plotWindow {tcpSource file} {
+global ns
+set time 0.01
+set now [$ns now]
+set cwnd [$tcpSource set cwnd_]
+puts $file "$now $cwnd"
+$ns at [expr $now+$time] "plotWindow $tcpSource $file" }
+$ns at 10.1 "plotWindow $tcp $windowVsTime2"  
+
+# Define node initial position in nam
+for {set i 0} {$i < $val(nn)} { incr i } {
+# 30 defines the node size for nam
+$ns initial_node_pos $node_($i) 30
+}
+
+# Telling nodes when the simulation ends
+for {set i 0} {$i < $val(nn) } { incr i } {
+    $ns at $val(stop) "$node_($i) reset";
+}
+
+# ending nam and the simulation 
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "stop"
+$ns at 150.01 "puts \"end simulation\" ; $ns halt"
+proc stop {} {
+    global ns tracefd namtrace
     $ns flush-trace
-    close $nf
-    exec nam out.nam &
+    close $tracefd
+    close $namtrace
+    exec nam simwrls.nam &
     exit 0
 }
 
-# Create five wireless nodes (n0, n1, n2, n3, n4)
-for {set i 0} {$i < 5} {incr i} {
-    set n($i) [$ns node]
-    $n($i) set X_ [expr 100 + ($i * 50)]   ;# Place nodes in a fixed pattern
-    $n($i) set Y_ 250
-    $n($i) set Z_ 0
-
-    $n($i) random-motion 0  ;# Disable mobility
-
-    # Set up wireless parameters for each node
-    $n($i) set channel_ $channel
-    $n($i) set prop_ $prop
-    $n($i) set netif_ $netif
-    $n($i) set mac_ $mac
-    $n($i) set ifq_ $ifq
-    $n($i) set ll_ $ll
-    $n($i) set antenna_ $ant
-    $n($i) set adhocRouting AODV
-}
-
-# Define the TCP connection (Node 3 → Node 4)
-set tcp [new Agent/TCP]
-$tcp set class_ 2
-$ns attach-agent $n(3) $tcp
-
-set sink [new Agent/TCPSink]
-$ns attach-agent $n(4) $sink
-$ns connect $tcp $sink
-$tcp set fid_ 1
-
-# Setup FTP over TCP
-set ftp [new Application/FTP]
-$ftp attach-agent $tcp
-$ftp set type_ FTP
-
-# Setup a UDP connection (Node 1 → Node 3)
-set udp [new Agent/UDP]
-$ns attach-agent $n(1) $udp
-
-set null [new Agent/Null]
-$ns attach-agent $n(3) $null
-$ns connect $udp $null
-$udp set fid_ 2
-
-# Setup CBR over UDP connection
-set cbr [new Application/Traffic/CBR]
-$cbr attach-agent $udp
-$cbr set type_ CBR
-$cbr set packet_size_ 1000
-$cbr set rate_ 1mb
-$cbr set random_ false
-
-# Schedule events for the CBR and FTP agents
-$ns at 0.1 "$cbr start"
-$ns at 1.0 "$ftp start"
-$ns at 4.0 "$ftp stop"
-$ns at 4.5 "$cbr stop"
-
-# Detach TCP and Sink agents
-$ns at 4.5 "$ns detach-agent $n(3) $tcp ; $ns detach-agent $n(4) $sink"
-
-# Call the finish procedure after 5 seconds
-$ns at 5.0 "finish"
-
-# Print CBR details
-puts "CBR packet size = [$cbr set packet_size_]"
-puts "CBR interval = [$cbr set interval_]"
-
-# Run the simulation
 $ns run
+
